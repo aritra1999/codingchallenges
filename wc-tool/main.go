aac8bc917c8150d5277b1c7172073177a3aa296f
Aritra Mondal <aritra041999.dev@gmail.com>
2023-12-19 22:42:24 +0100

    Functionality to read command line flags

--- wc-tool/main.go
+++ wc-tool/main.go
@@ -1,7 +1,24 @@
 package main
 
 import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
 
 func main() {
 	fmt.Println("Hello, World!")
+	countWords := flag.Bool("w", false, "bool flag to count words")
+	countLines := flag.Bool("l", false, "bool flag to count lines")
+	countBytes := flag.Bool("m", false, "bool flag to count bytes")
+	countChars := flag.Bool("c", false, "bool flag to count chars")
+
+	flag.Parse()
+
+	fmt.Println("countWords:", *countWords)
+	fmt.Println("countLines:", *countLines)
+	fmt.Println("countBytes:", *countBytes)
+	fmt.Println("countChars:", *countChars)
+
 }
