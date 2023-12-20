2af34b8dca30a2cf854d6602dc167bb41540ed77
Aritra Mondal <aritra041999.dev@gmail.com>
2023-12-20 08:43:11 +0100

    Add unit test for CountLines

--- wc-tool/main_test.go
+++ wc-tool/main_test.go
@@ -1,6 +1,10 @@
 package main
 
 import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestCountBytes(t *testing.T) {
 	input := []byte("Hello, World!")
@@ -11,6 +15,22 @@ func TestCountBytes(t *testing.T) {
 		t.Errorf("CountBytes(%q) = %d, want %d", input, result, expected)
 	} else {
 		t.Logf("CountBytes(%q) = %d, want %d", input, result, expected)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	inputLine1 := []byte("Hello, World!This is a new line")
+	inputLine2 := []byte("This is a new line")
+
+	input := []byte(fmt.Sprintf("%s\n %s\n", inputLine1, inputLine2))
+
+	expected := 2
+	result := CountLines(input)
+
+	if result != expected {
+		t.Errorf("CountLines(%q) = %d, want %d", input, result, expected)
+	} else {
+		t.Logf("CountLines(%q) = %d, want %d", input, result, expected)
 	}
 }
 
